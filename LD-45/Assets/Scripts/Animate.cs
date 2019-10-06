3695cd6c0bfb89e075688a312d5c15645b7afa2f
Konosprod <konosprod@free.fr>
2019-10-06 12:51:29 +0200

    Created public, update healtbar with animation, set icons in the fighting scenes, desync animation also
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ LD-45/Assets/Scripts/Animate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+
+    public Animator animator;
+    public float minTime = 0f;
+    public float maxTime = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForAnimation());
+    }
+
+    private IEnumerator WaitForAnimation()
+    {
+        float time = Random.Range(minTime, maxTime);
+        yield return new WaitForSeconds(time);
+        animator.enabled = true;
+    }
+}
---
+++ LD-45/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image health;
+    public float speed = 1f;
+    public float time = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHealthBar(float value)
+    {
+        StartCoroutine(decreaseHealth(value));
+    }
+
+    IEnumerator decreaseHealth(float value)
+    {
+        float elapsedTime = 0;
+
+        while(elapsedTime < time)
+        {
+            health.fillAmount = Mathf.Lerp(health.fillAmount, value, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
